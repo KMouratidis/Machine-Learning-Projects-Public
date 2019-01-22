0a0834947e00d2548cb0398bc0792c00d5b49604
Konstantinos Mouratidis <k.mouratidis@hotmail.com>
2019-01-23 01:05:23 +0200

    Tank simulation can now handle pseudo-animation, it also has a bot strategy, and can report back metrics.

--- Tank Warfare Simulation/tank_simulation.py
+++ Tank Warfare Simulation/tank_simulation.py
@@ -6,6 +6,7 @@ from PIL import Image
 import numpy as np
 import random
 import warnings
+import os
 
 # https://stackoverflow.com/a/45076236/6655150
 from matplotlib.colors import ListedColormap 
@@ -33,19 +34,45 @@ class Game:
 
 class Turn:
     
-    def __init__(self, instructions):
+    # tank speed, how much they can move per turn
+    _speed = 3
+    
+    def __init__(self, ):
+        pass
+    
+    def turn(self, instructions):
         """
-            orders: a dictionary of orders: {general:[[acquire_tank, {'T': T}], [order_tank, dict(**kwargs)], [order3...]]}
+            orders: a dictionary of orders: {general:[[acquire_tank, {'T': T}],
+                                                      [order_tank, dict(**kwargs)],
+                                                      [order3...]]}
         """
         for general, orders in instructions.items():
             for order in orders:
+#                 try:
                 if order[0] == "acquire_tank":
                     general.acquire_tank(**order[1]) # T
                 elif order[0] == "order_tank":
-                    general.order_tank(**order[1]) # T, pos
+                    T = order[1]["T"]
+                    nx, ny = order[1]["pos"]
+
+                    # if ordered too far, adjust apropriately
+                    if distance((nx,ny), (T.x, T.y)) > Turn._speed:
+                        if (T.x - nx) > (T.y - nx):
+                            nx = np.sign(nx - T.x) * 2 + T.x
+                            ny = np.sign(ny - T.y) * 1 + T.y
+                        else:
+                            nx = np.sign(nx - T.x) * 1 + T.x
+                            ny = np.sign(ny - T.y) * 2 + T.y
+
+                    general.order_tank(T, (nx, ny)) # T, pos
                 elif order[0] == "purchase_tanks":
                     general.purchase_tanks(**order[1]) # any of the valid kwargs
+                else:
+                    print("Command not recognized.")
+#                 except Exception as e:
+#                     print("An error occurred, log:", e)
 
+        return m.plot()
 
 class General:
     
@@ -64,7 +91,10 @@ class General:
     ### Available commands (to be used by Turn) ###    
     
     def acquire_tank(self, T):
+        # mainly used to get tanks from other generals / tansfer ownership
         self.tanks.append(T)
+        T.general.tanks.remove(T)
+        T.general = self
         
     def purchase_tanks(self, n=5, *, type_="StuG", types=None):
         if types is None:
@@ -144,6 +174,7 @@ class Patch:
         self.y = y
         self.residents = []
         self.map = map_
+        self.color = "white"
         
     def __str__(self):
         return f"Patch ({self.x}, {self.y}), [{len(self.residents)}]"
@@ -152,10 +183,10 @@ class Patch:
         self.residents.append(T) # add tank to patch
         
         # see if enemy tanks are in the same patch
-        # if so, kill new or old one with 50% probabilty
+        # if so, kill the one farther from its comrades with 66% probabilty
         for tank in self.residents:
             if tank.country != T.country:
-                tank_to_kill = random.choice([tank, T])
+                tank_to_kill = random.choice([tank, T, tank if self.color==tank.country.color else T])
                 if tank_to_kill.general is not None:
                     tank_to_kill.general.tanks.remove(tank_to_kill)
                     tank_to_kill.general = None
@@ -185,6 +216,7 @@ class Map:
         self.size = size
         self.map = [[Patch(x, y, self) for y in range(1, size+1)] for x in range(1,size+1)]
         self.tanks = []
+        self.map_score = 0
     
     def __str__(self):
         s = ""
@@ -197,14 +229,15 @@ class Map:
     
     def add_tank(self, T, x=1, y=1):
         self.map[y-1][x-1].append(T) # this appends a tank to a patch of the map
-        self.tanks.append(T)
+        self.tanks.append(T) # adds the tank to the map's list of tanks
             
     def to_array(self):
         return np.array([[len(patch.residents) for patch in row] for row in self.map])
 
-    def plot(self, *, k=3):
-        fig, ax = plt.subplots(figsize=(12,8)) 
-
+    def plot(self, *, k=3, scale=2):
+        fig, ax = plt.subplots(figsize=(16,11)) 
+    
+        # plot tanks
         for tank in self.tanks:
             if tank.country.color == "blue":
                 tank_im = im_blue
@@ -212,27 +245,28 @@ class Map:
                 tank_im = im_red
                 
             ax.imshow(tank_im, aspect='auto',
-                          extent=(tank.x+0.5, tank.x-0.5, tank.y-0.5, tank.y+0.5),
+                          extent=(tank.x*scale+0.5, tank.x*scale-0.5, tank.y*scale-0.5, tank.y*scale+0.5),
                           zorder=2)
-            attack_range = plt.Circle((tank.x, tank.y), 2,
+            attack_range = plt.Circle((tank.x*scale, tank.y*scale), 2,
                                       color=tank.country.color, fill=False)
             ax.add_artist(attack_range)
             
-            line_of_sight = plt.Circle((tank.x, tank.y), 4,
+            line_of_sight = plt.Circle((tank.x*scale, tank.y*scale), 4,
                                        color=tank.country.color, fill=False, alpha=0.5)
             ax.add_artist(line_of_sight)
 
         for row in self.map:
             for p in row:
                 if len(p.residents)>=1:
-                    ax.text(p.x+0.3, p.y+0.3, f'{len(p.residents)}',)
+                    ax.text(p.x*scale+0.3, p.y*scale+0.3, f'{len(p.residents)}',)
 
-        ax.imshow(np.zeros((self.size, self.size)), cmap="gray_r")
+        # plot base map
+        ax.imshow(np.zeros((self.size*scale, self.size*scale)), cmap="gray_r")
         
         
         ## Plot area of influence
         if len(self.tanks)>=1:
-            coords = np.array([(t.x, t.y) for t in self.tanks])
+            coords = np.array([(t.x*scale, t.y*scale) for t in self.tanks])
             countries = np.array([t.country.color for t in self.tanks])
 
             # K=3 nearest neighbor
@@ -244,93 +278,89 @@ class Map:
             else:
                 col = np.array([countries[d][0] for d in distances])
             
+            # create an array for the color mesh
             col = np.array([1 if v=="blue" else 0 for v in col])
+            col = col.reshape((self.size*scale+1, self.size*scale+1))
+            
+            self.map_score = 0
+            # assign colors to patches
+            for yi in range(self.size):
+                for xi in range(self.size):
+                    self.map[yi][xi].color = "blue" if col[yi][xi] else "red"
+                    self.map_score += 2* (col[yi][xi]-0.5) # plus (blue) or minus (red) one
 
-            ax.pcolormesh(xx, yy, col.reshape((self.size+1, self.size+1)),
+            ax.pcolormesh(xx, yy, col,
                           cmap=cmap_light, vmin=0, vmax=1.)
         
         
-        ax.set_xlim(1, self.size)
-        ax.set_xticks(np.arange(1, self.size+1))
-        ax.set_ylim(1, self.size)
-        ax.set_yticks(np.arange(1, self.size+1))
+        ax.set_xlim(1, self.size*scale)
+        ax.set_xticks(np.arange(1, self.size*scale+1, 2))
+        ax.set_xticklabels(np.arange(1, self.size*scale+1))
+        ax.set_ylim(1, self.size*scale)
+        ax.set_yticks(np.arange(1, self.size*scale+1, 2))
+        ax.set_yticklabels(np.arange(1, self.size*scale+1))
+        
+        # handle a grid
+        for x in range(self.size):
+            ax.axhline(x*scale+0.5*scale, color="k", alpha=0.6)
+            ax.axvline(x*scale+0.5*scale, color="k", alpha=0.6)
+
+        return fig
 
 
-        plt.show()
 
 if __name__ == '__main__':
     n = 25
+    scale=2
     m = Map(n)
+    t = Turn()
 
-    xx, yy = np.meshgrid(list(range(0,n+1)), list(range(0,n+1)))
+    xx, yy = np.meshgrid(list(range(0,n*scale+1)), list(range(0,n*scale+1)))
     xr, yr = xx.ravel(), yy.ravel()
 
     red = Country("red")
     blue = Country("blue")
 
     Rommel = General("Rommel", red, map_=m, pos=(2,2))
-    Montgomery = General("Montgomery", blue, map_=m, pos=(12,12))
+    Montgomery = General("Montgomery", blue, map_=m, pos=(20,20))
 
-    t = Tank("StuG", country=red)
-    m.add_tank(t, 3,3)
-
-    t.move(2,2)
-    m.plot()
-
-    t2 = Tank("Panzer", country=blue)
-    m.add_tank(t2, 6,6)
-    m.plot()
-
-    t2.move(4,4)
-    m.plot()
-
-    t2.move(2,2)
-    m.plot()
-
-    if t2.patch is not None:
-        t2.move(7,7)
-    else:
-        t.move(7,7)
-    m.plot()
+    try:
+        os.mkdir("simulation")
+    except:
+        pass
+    m.plot().savefig(f"simulation/frame-{str(0).zfill(3)}.png")
 
     Rommel.purchase_tanks(n=5)
     Montgomery.purchase_tanks(n=3)
-    m.plot()
 
-    Rommel.order_tank(Rommel.tanks[0], (3,3))
-    m.plot()
+    m.plot().savefig(f"simulation/frame-{str(1).zfill(3)}.png")
 
     instructions = {
-        Rommel : [["order_tank", {'T': Rommel.tanks[0], 'pos': (5,8)}],
-                  ["order_tank", {'T': Rommel.tanks[1], 'pos': (6,9)}]
-                 ],
-        Montgomery: [["order_tank", {'T': Montgomery.tanks[0], 'pos': (18,19)}],
-                     ["order_tank", {'T': Montgomery.tanks[1], 'pos': (15,17)}]
+        Rommel: [["order_tank", {'T': Rommel.tanks[0], 'pos': (18,23)}],
                     ],
-    }
-    Turn(instructions=instructions)
-    m.plot()
-
-
-    instructions = {
-        Rommel : [["order_tank", {'T': Rommel.tanks[0], 'pos': (8,11)}],
-                  ["order_tank", {'T': Rommel.tanks[1], 'pos': (9,10)}]
-                 ],
-        Montgomery: [["order_tank", {'T': Montgomery.tanks[0], 'pos': (14,12)}],
-                     ["order_tank", {'T': Montgomery.tanks[1], 'pos': (10,11)}]
+        Montgomery: [["order_tank", {'T': Montgomery.tanks[0], 'pos': (10,10)}],
                     ],
+        
     }
-    Turn(instructions=instructions)
-    m.plot()
+
+    t.turn(instructions=instructions).savefig(f"simulation/frame-{str(2).zfill(3)}.png")
 
 
-    instructions = {
-    Montgomery: [["order_tank", {'T': Montgomery.tanks[2], 'pos': (7,18)}],
-                ],
-    }
-    Turn(instructions=instructions)
-    m.plot()
+    # calculate distances from enemy's tanks
+    def bot_instructions():
+        ds = [distance((Rommel.tanks[-1].x, Rommel.tanks[-1].x), (t.x, t.y)) for t in Montgomery.tanks]
+        closest_enemy_tank = Montgomery.tanks[np.argmin(ds)]
 
+        Bot_instructions = {
+            Rommel : [["order_tank", {'T': t, 'pos': (closest_enemy_tank.x, closest_enemy_tank.y)}]
+                       for t in Rommel.tanks]}
+        
+        return Bot_instructions
 
-    m.plot(k=1) # who can occupy first
+    t.turn(bot_instructions()).savefig(f"simulation/frame-{str(3).zfill(3)}.png")
 
+    i = 4
+    while (Rommel.tanks) and (Montgomery.tanks):
+        fig = t.turn(bot_instructions())
+        fig.savefig(f"simulation/frame-{str(i).zfill(3)}.png")
+        i += 1
