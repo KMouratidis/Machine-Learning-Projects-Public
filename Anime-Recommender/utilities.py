17979fdbb3bd3d4d5e26ba7fe937d1baf39150a9
Konstantinos Mouratidis <k.mouratidis@hotmail.com>
2019-03-03 11:44:39 +0200

    Reworked the recommender (still experimenting) to allow for use with other datasets by abstracting utility functions. See Recommender3 for current functions. Part of this is due to wanting to implement formulas from scratch.

--- Anime-Recommender/utilities.py
+++ Anime-Recommender/utilities.py
@@ -9,13 +9,19 @@ with open("anime_list.txt") as f:
     anime_list = f.read()
     anime_list = anime_list.split("\n")[:-1] # last one is an empty line
 
-def load_dataset(min_episodes=6, min_year=1999, min_score='avg',
+def load_anime_dataset(min_episodes=6, min_year=1999, min_score='avg',
                  scored_by=4999, min_ep_duration=7, drop_second_seaons=False,
-                 normalize_users=True, scaling="min_max",
+                 normalize=None, scaling="min_max", min_ratings=10,
                  also_create_features=True, text_features=False,
                  save=False):
     """
+    This is a dataset-specific loading utility that takes the data from 
+    a database and returns a user-anime(rating) pivot table. If you also
+    set also_create_features then you get another anime-feature(score) table.
+
     min_score: 'avg' or float.
+    normalize: str ("user", "item") or None
+    min_ratings: int, how maminimum number of ratings per user
     scaling: 'min_max' or 'zero_mean'.
     """
 
@@ -32,23 +38,32 @@ def load_dataset(min_episodes=6, min_year=1999, min_score='avg',
 
     df = pd.read_sql(query, conn)
     # Each user should rate at least 10 anime
-    # Typecast to float32 to save memory
-    df = df.pivot_table("my_score", "username", "title").dropna(thresh=10,
-                                                                axis=0).astype(np.float32)
-    # For every user, rate each anime with the user average
-    # conversion of boolean to 0/1
-    df = df.apply(lambda x: x.fillna(x.mean()), axis=1*(not normalize_users))
-    # For every user we are subtracting the mean (if normalize_users is True)
-    if scaling == "zero_mean":
-        df = df.apply(lambda x: x - x.mean(), axis=1)
-    elif scaling == "min_max":
-        # For every user subtract the minimum and divide by the maximum
-        # so we get data in the range of 0-1 which can be interpreted
-        # as a percentage rating
-        df = df.apply(lambda x: x - x.min(), axis=1*normalize_users)
-        df = df.apply(lambda x: x / x.max(), axis=1*normalize_users)
+    # Typecast to float16 to save memory
+    df = df.pivot_table(values="my_score", index="username", columns="title").dropna(thresh=min_ratings,
+                                                                axis=0).astype(np.float16)
+    
+    if normalize is not None:
+        if normalize == "user":
+            # For every user, rate each anime with the user average
+            axis = 0
+        elif normalize == "item":
+            axis = 1
+
+        df = df.apply(lambda x: x.fillna(x.mean()), axis=axis)
+
+        # For every user we are subtracting the mean (if normalize_users is True)
+        if scaling == "zero_mean":
+            df = df.apply(lambda x: x - x.mean(), axis=int(not axis))
+        elif scaling == "min_max":
+            # For every user subtract the minimum and divide by the maximum
+            # so we get data in the range of 0-1 which can be interpreted
+            # as a percentage rating
+            df = df.apply(lambda x: x - x.min(), axis=int(not axis))
+            df = df.apply(lambda x: x / x.max(), axis=int(not axis))
+        else:
+            print("[IMPORTANT INFO]: No scaling!")
     else:
-        print("[IMPORTANT INFO]: No scaling!")
+        print("[IMPORTANT INFO]: No normalization or scaling!")
 
     if drop_second_seaons:
         # drop (some) second seasons
